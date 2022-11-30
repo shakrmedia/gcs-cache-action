7a6b60910e61a76e83d851f46a7ac68702726f99
Minku Lee <public@premi.st>
2022-11-30 11:44:44 +0900

    typescript

--- src/restore.ts
+++ src/restore.ts
@@ -3,21 +3,23 @@ import * as exec from '@actions/exec'
 
 async function run(): Promise<void> {
   try {
-    const bucket = process.env.GCS_CACHER_BUCKET;
-    if(bucket === undefined) { return; }
+    const bucket = process.env.GCS_CACHER_BUCKET
+    if (bucket === undefined) {
+      return
+    }
 
-    const path: string = core.getInput('path');
-    const key: string = core.getInput('key');
-    const restore_keys: string[] = core.getMultilineInput('restore_keys');
+    const path: string = core.getInput('path')
+    const key: string = core.getInput('key')
+    const restore_keys: string[] = core.getMultilineInput('restore_keys')
 
-    const reducer = (arr: string[], item: string) => [...arr, '-restore', item];
-    const restore_args = restore_keys.reduce(reducer, ['-restore', key]);
+    const reducer = (arr: string[], item: string): string[] => [...arr, '-restore', item]
+    const restore_args = restore_keys.reduce(reducer, ['-restore', key])
 
     await exec.exec(
       'gcs-cacher',
       ['-bucket', bucket, ...restore_args, '-dir', path],
-      { ignoreReturnCode: true }
-    );
+      {ignoreReturnCode: true}
+    )
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
